3a465228cd4b2a5c0ce7bb7fa59fa434092843d8
Dmitry Sayko <gromov2300@gmail.com>
2024-08-22 03:34:54 +0300

    add interactive and js plugin, also add responsive to 431, 768 width.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayTimeout: 6500,
+    autoplaySpeed: 1000,
+    loop: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        762:{
+            items:2
+        },
+        992:{
+            items:2
+        },
+        1022:{
+            items:2
+        },
+        1100:{
+            items:3
+        }
+    }
+
+});
